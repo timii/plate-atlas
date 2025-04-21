833e246d700c52b7e56982fe0b0974e46bf66bde
timii <tim-t@hotmail.de>
2025-04-21 21:12:36 +0200

    fix: disable eslint rule for two word vue files

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,6 +12,7 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: { 'vue/multi-word-component-names': 'off' },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
