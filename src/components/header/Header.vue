16f0f62b49a9217816e80ec0e82f4c7f00c82bb0
timii <tim-t@hotmail.de>
2025-04-25 22:09:52 +0200

    feat: create header component

---
+++ src/components/header/Header.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header
+    class="fixed flex w-full items-center justify-between px-4 sm:px-6 h-14 dark:bg-background-dark-default border-b border-border-light dark:border-border-dark"
+  >
+    <div class="header-left flex gap-6">
+      <div>Logo (TBD)</div>
+      <nav>
+        <RouterLink class="pr-6" to="/">Overview</RouterLink>
+        <RouterLink class="" to="/map">Map</RouterLink>
+      </nav>
+      <div>Search</div>
+    </div>
+    <div class="header-right flex gap-6">
+      <div>About</div>
+      <div>Toggle</div>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
