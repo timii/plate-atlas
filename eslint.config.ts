b4302f6e05942ec9ca1f4903e0666ef3f0182a87
timii <tim-t@hotmail.de>
2025-05-06 21:58:22 +0200

    build: allow single word vue files

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,15 +9,16 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-    rules: { 'vue/multi-word-component-names': 'off' },
-  },
-
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+
+  // allow single word vue files
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: { 'vue/multi-word-component-names': 'off' },
+  },
 )
