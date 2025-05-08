3e0c87a96819c22ca64de79054fce9e7e45230f0
timii <tim-t@hotmail.de>
2025-05-08 21:48:34 +0200

    feat: load in countries json and date of last update

--- src/components/overview/Overview.vue
+++ src/components/overview/Overview.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
+import countriesJson from '../../data/current-license-plates.json'
+
+const lastUpdated = computed(() => {
+  const date = new Date(countriesJson.lastUpdate)
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  const formatted = date.toLocaleDateString(undefined, options)
+  return formatted
+})
 
 onMounted(() => {
-  console.log('Overview mounted')
+  console.log('Overview mounted -> json:', countriesJson)
 })
 </script>
 
 <template>
   <div class="h-full w-full flex flex-col items-center justify-center pt-6 border">
+    <div class="text-xs">Last Updated: {{ lastUpdated }}</div>
     <h1 class="title text-4xl mb-8">Countries</h1>
     <div class="content flex flex-col w-4/5 items-center justify-center gap-6">
       <div class="actions flex justify-between gap-6 w-full bg-amber-800">
