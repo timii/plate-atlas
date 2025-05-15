2492fcda940d729dc216e73054cd23f7e1ec7425
timii <tim-t@hotmail.de>
2025-05-15 21:56:55 +0200

    feat: add some other versions to test CountryCard styling

--- src/components/overview/CountryCard.vue
+++ src/components/overview/CountryCard.vue
@@ -10,15 +10,76 @@ const props = defineProps({
 })
 </script>
 
-<template>
+<!-- Version 1 -->
+<!-- <template>
   <div
-    class="country-card flex flex-row rounded pr-2 gap-2 items-center bg-background-dark-highlight shadow-sm shadow-background-dark-highlight-layer2"
+    class="country-card flex flex-row rounded pr-2 gap-2 items-center bg-background-dark-highlight shadow-sm shadow-background-dark-highlight-layer2 cursor-pointer"
   >
     <div class="flex flex-row gap-2 items-center bg-background-dark-highlight-layer2 p-2">
       <img :src="props.country.flagThumb" :alt="'flag'" />
       <div>{{ props.country.code }}</div>
     </div>
     <div>{{ props.country.country }}</div>
+  </div>
+</template> -->
+
+<!-- Version 2 -->
+<!-- <template>
+  <div
+    class="country-card flex flex-row rounded pr-2 gap-2 items-center bg-background-dark-highlight shadow-sm shadow-background-dark-highlight-layer2 cursor-pointer"
+  >
+    <div
+      class="flex flex-row gap-2 items-center bg-background-dark-highlight-layer2 p-2 text-xl font-bold"
+    >
+      <div>{{ props.country.code }}</div>
+    </div>
+    <div>{{ props.country.country }}</div>
+    <div class="image-container">
+      <img :src="props.country.flagThumb" :alt="'flag'" />
+    </div>
+  </div>
+</template> -->
+
+<!-- Version 3 -->
+<!-- <template>
+  <div
+    class="country-card flex flex-row rounded pr-2 gap-2 items-center bg-background-dark-highlight shadow-sm shadow-background-dark-highlight-layer2 cursor-pointer"
+  >
+    <div
+      class="flex flex-row gap-2 items-center bg-background-dark-highlight-layer2 p-2 text-xl font-bold h-full border-r rounded-[inherit] border-gray-600"
+    >
+      <div>{{ props.country.code }}</div>
+    </div>
+    <div class="flex flex-col leading-5">
+      <div>{{ props.country.country }}</div>
+      <div class="text-sm italic text-gray-400">{{ props.country.continent }}</div>
+    </div>
+    <div class="image-container ml-2 max-h-6 max-w-7">
+      <img :src="props.country.flagThumb" :alt="'flag'" />
+    </div>
+  </div>
+</template> -->
+
+<!-- Version 4 -->
+<template>
+  <div
+    class="country-card flex flex-row rounded pr-2 gap-2 items-center bg-background-dark-highlight shadow-sm shadow-background-dark-highlight-layer2 cursor-pointer h-12"
+  >
+    <div
+      class="flex flex-row gap-2 items-center bg-background-dark-highlight-layer2 p-2 text-xl font-bold h-full border-r rounded-[inherit] border-gray-600"
+    >
+      <div>{{ props.country.code }}</div>
+    </div>
+    <div class="flex flex-col leading-5">
+      <div>{{ props.country.country }}</div>
+      <div class="text-sm italic text-gray-400">{{ props.country.continent }}</div>
+    </div>
+    <div class="flex flex-col leading-5 items-center">
+      <div class="image-container ml-2 max-h-6 max-w-7">
+        <img :src="props.country.flagThumb" :alt="'flag'" />
+      </div>
+      <div class="text-sm italic text-gray-400">{{ props.country.fromYear }}</div>
+    </div>
   </div>
 </template>
 
