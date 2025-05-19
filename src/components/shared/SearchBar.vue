decbf36e7df2579513ff3d82c9686950ae896dc4
timii <tim-t@hotmail.de>
2025-05-19 21:24:37 +0200

    fix: input focus styling

--- src/components/shared/SearchBar.vue
+++ src/components/shared/SearchBar.vue
@@ -8,7 +8,7 @@ const text = defineModel('text', { type: String, default: '' })
       v-model="text"
       type="text"
       placeholder="Search..."
-      class="border text-text-light-default border-border-light3 text-sm rounded-lg block w-full p-2.5 dark:bg-background-dark-highlight dark:border-border-dark3 dark:placeholder-gray-400 dark:text-text-dark-default"
+      class="border text-text-light-default border-border-light3 text-sm rounded-lg block w-full p-2.5 focus:outline-none dark:bg-background-dark-highlight dark:border-border-dark3 dark:placeholder-gray-400 dark:text-text-dark-default"
     />
   </div>
 </template>
