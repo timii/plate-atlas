2dfad76a33e4513be45df654c9ca5094d6215feb
timii <tim-t@hotmail.de>
2025-05-21 21:49:58 +0200

    feat: add second chevron svg for open dropdown

--- src/components/shared/Dropdown.vue
+++ src/components/shared/Dropdown.vue
@@ -19,10 +19,23 @@ function toggleMenu() {
       >
         Sort by
         <svg
+          v-if="menuOpen"
+          class="-mr-1 size-5 rotate-180 text-gray-400"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <svg
+          v-else
           class="-mr-1 size-5 text-gray-400"
           viewBox="0 0 20 20"
           fill="currentColor"
-          aria-hidden="true"
           data-slot="icon"
         >
           <path
