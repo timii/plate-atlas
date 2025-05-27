ffaf97aa9224139890f9a251cc1d57b02f00ce91
timii <tim-t@hotmail.de>
2025-05-27 21:47:26 +0200

    feat: filter countries by search term

--- src/stores/countries.ts
+++ src/stores/countries.ts
@@ -13,10 +13,17 @@ export const useCountriesStore = defineStore('countries', () => {
   const mappedCountries = computed(() => {
     console.log('mappedCountries -> searchTerm:', searchTerm.value, 'sortBy:', sortBy.value)
     const countriesCopy = countries.value.countries
-    const countriesLength = countriesCopy.length
-    const slice = countriesCopy.slice(0, randomIntFromInterval(1, countriesLength))
-    console.log('mappedCountries return:', slice)
-    return slice
+    // const countriesLength = countriesCopy.length
+    // const slice = countriesCopy.slice(0, randomIntFromInterval(1, countriesLength))
+
+    // filter countries by search term
+    const filtered = countriesCopy.filter((countryObj) => {
+      const countryName = countryObj.country.toLowerCase()
+      return countryName.includes(searchTerm.value.toLowerCase())
+    })
+
+    console.log('mappedCountries return:', filtered)
+    return filtered
   })
 
   return { countries, searchTerm, sortBy, mappedCountries }
