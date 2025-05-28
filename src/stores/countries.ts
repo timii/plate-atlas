f0820280607002afaefed39c574f7465edf01e7c
timii <tim-t@hotmail.de>
2025-05-28 08:24:47 +0200

    feat: implement sort countries logic

--- src/stores/countries.ts
+++ src/stores/countries.ts
@@ -1,6 +1,5 @@
 import { type ICountryData } from '@/models/country.model'
 import { SortBy } from '@/models/dropdown.model'
-import { randomIntFromInterval } from '@/utils/utils'
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
@@ -12,6 +11,7 @@ export const useCountriesStore = defineStore('countries', () => {
   // return list of countries after being filtered and sorted
   const mappedCountries = computed(() => {
     console.log('mappedCountries -> searchTerm:', searchTerm.value, 'sortBy:', sortBy.value)
+
     const countriesCopy = countries.value.countries
     // const countriesLength = countriesCopy.length
     // const slice = countriesCopy.slice(0, randomIntFromInterval(1, countriesLength))
@@ -22,8 +22,21 @@ export const useCountriesStore = defineStore('countries', () => {
       return countryName.includes(searchTerm.value.toLowerCase())
     })
 
-    console.log('mappedCountries return:', filtered)
-    return filtered
+    // sort filtered countries by current sort by value
+    const sorted = filtered.sort((a, b) => {
+      // sort alphabetically in descending order
+      if (sortBy.value === SortBy.ALPHABETIC_DESC) {
+        return b.country.localeCompare(a.country)
+      }
+
+      // sort alphabetically in asecnding order
+      else {
+        return a.country.localeCompare(b.country)
+      }
+    })
+
+    console.log('mappedCountries return:', sorted)
+    return sorted
   })
 
   return { countries, searchTerm, sortBy, mappedCountries }
