7d49df8474fe771886180ebb1ba6f6686579cac6
timii <tim-t@hotmail.de>
2025-06-03 21:46:18 +0200

    feat: import area codes json in detail view

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import areaCodesJson from '../data/country-area-codes.json'
 
 const route = useRoute()
 
+const details = ref({})
+
 onMounted(() => {
   console.log('onMounted in Detail called -> code:', route.params.code)
-  // TODO: dynamically import country details using code
+  // TODO: dynamically import country details using code (always import test country for testing layout and necessary data fields)
+  // TODO: add interface for json
+  details.value = areaCodesJson[route.params.code as string]
+  console.log('onMounted in Detail:', details.value)
 })
 </script>
 
 <template>
   <div>Detail page for {{ $route.params.code }}</div>
+  <div>{{ details.country }}</div>
+  <div class="sections flex flex-col gap-4">
+    <div v-for="section in details.sections" :key="section">
+      <div class="text-xl">{{ section.title }}</div>
+      <div class="areas flex flex-wrap gap-4">
+        <div v-for="area in section.areas" :key="area.code" class="area">
+          {{ area }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
