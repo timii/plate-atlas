adf252a8cc42dfb1ba586c1d4b09e20922aa6d11
timii <tim-t@hotmail.de>
2025-10-26 21:04:42 +0100

    feat: add generic icon button component

---
+++ src/components/shared/IconButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import StarEmpty from '@/assets/icons/StarEmpty.vue'
+import StarFilled from '@/assets/icons/StarFilled.vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <!-- ------------------------------- -->
+  <!-- ------------------------------- -->
+  <!-- ------------------------------- -->
+  <!-- TODO: set correct color depending on color scheme -->
+  <!-- TODO: use icon name from props for compoent -->
+  <!-- ------------------------------- -->
+  <!-- ------------------------------- -->
+  <!-- ------------------------------- -->
+  <div v-if="props.icon" class="text-white">
+    <!-- <div>icon - {{ props.icon }}</div> -->
+    <!-- <component :is="props.icon"></component> -->
+    <StarEmpty />
+    <StarFilled />
+  </div>
+</template>
+
+<style scoped></style>
