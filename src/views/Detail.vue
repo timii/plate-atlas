b638234994ba4ad82f44502b4826a2a8dce67a42
timii <tim-t@hotmail.de>
2026-01-03 21:27:15 +0100

    feat: add info box for countries without area codes

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -13,6 +13,7 @@ import StarFilled from '@/assets/icons/StarFilled.vue'
 import Loading from '@/components/shared/Loading.vue'
 import { useDetailsStore } from '@/stores/details'
 import { storeToRefs } from 'pinia'
+import Info from '@/assets/icons/Info.vue'
 
 const route = useRoute()
 const countriesStore = useCountriesStore()
@@ -154,18 +155,27 @@ onMounted(async () => {
     <div v-else class="content flex w-4/5 flex-col items-center justify-center gap-8">
       <template v-if="exampleImages && exampleImages.length > 0">
         <div
+          class="flex items-center gap-4 rounded-lg border border-border-light3 px-6 py-4 text-lg text-text-light-default dark:border-border-dark3 dark:bg-background-dark-highlight dark:text-text-dark-default dark:placeholder-gray-400"
+        >
+          <Info></Info>
+          <span>
+            License plates in {{ countryName ?? 'this country' }} are not tied to specific regions
+            or area codes
+          </span>
+        </div>
+        <div
           class="flex w-full flex-col items-center justify-center gap-4"
           v-for="imageCategory in exampleImages"
           :key="imageCategory.category"
         >
-          <h2 class="text-center text-xl">{{ imageCategory.category }}</h2>
+          <h2 class="text-center text-lg">{{ imageCategory.category }}</h2>
           <div class="flex max-w-full flex-wrap items-center justify-center gap-4">
             <template v-for="imageObj in imageCategory.images" :key="imageObj.url">
               <div class="flex flex-col">
                 <img
                   :src="imageObj.url"
                   :alt="`Example image for ${imageObj.title ?? 'license plate'}`"
-                  class="max-h-40 max-w-64 object-contain"
+                  class="max-h-72 max-w-64 object-contain"
                 />
                 <span class="text-center text-base text-text-light-info dark:text-text-dark-info">{{
                   imageObj.title
